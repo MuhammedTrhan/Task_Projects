71b75ea7c01001341a1ee5e691b0c71f5cd44a9a
Muhammed Turhan <meheme4434@gmail.com>
2026-05-23 15:28:35 +0200

    UPDATED enemies to rotate to the direction and play animation

--- dodge-the-creeps/Scripts/enemy.gd
+++ dodge-the-creeps/Scripts/enemy.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+@onready var sprite: AnimatedSprite2D = $EnemySprite
+
 const SPEED = 150.0
 
 var target_position: Vector2 = Vector2.ZERO
@@ -11,10 +13,16 @@ var is_active: bool = true
 
 func _physics_process(_delta: float) -> void:
 	if is_active and target_assigned:
-		# Move towards the target location
+		# Find the direction vector towards the target position and normalize it
 		var direction = (target_position - position).normalized()
+
+		# Rotate the enemy to face the direction it's moving
+		sprite.rotation = direction.angle()
+
+		# Move the enemy towards the target position
 		velocity = direction * SPEED
 		move_and_slide()
+		sprite.play("swim")
 
 		# Check if the enemy has reached the target position
 		if position.distance_to(target_position) < 10.0:
@@ -27,6 +35,7 @@ func recycle_to_pool() -> void:
 	# Mark the enemy as inactive and hide it
 	is_active = false
 	target_assigned = false
+	sprite.stop()
 	hide()
 
 	# Disable its inner Area2D collision shape so it stops hurting the player
