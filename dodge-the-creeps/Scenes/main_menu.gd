70af02df00099627186cc85e81fc79fd9bd14865
Muhammed Turhan <meheme4434@gmail.com>
2026-05-24 21:41:08 +0200

    ADDED main menu sccene

---
+++ dodge-the-creeps/Scenes/main_menu.gd
@@ -0,0 +1,22 @@
+extends Control
+
+
+const GAME_SCENE_PATH = "res://Scenes/main_scene.tscn"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file(GAME_SCENE_PATH)
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
