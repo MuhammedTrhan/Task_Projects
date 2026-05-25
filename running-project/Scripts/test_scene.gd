077c6163fbf310d01830971e6376433ea7187e79
Muhammed Turhan <meheme4434@gmail.com>
2026-05-25 17:56:28 +0200

    ADDED customization of cursor and ADDED player node

--- running-project/Scripts/test_scene.gd
+++ running-project/Scripts/test_scene.gd
@@ -1,18 +1,1 @@
 extends Node2D
-
-@export var rotation_speed: float = 1.0
-
-@onready var area_1 = $Coin
-@onready var area_2 = $Area2D2
-@onready var area_3 = $Area2D3
-
-
-func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("a_pressed"):
-		area_1.modulate = Color.from_hsv(randf(), 0.8, 1.0)
-
-	if event.is_action_pressed("s_pressed"):
-		area_2.modulate = Color.from_hsv(randf(), 0.8, 1.0)
-
-	if event.is_action_pressed("d_pressed"):
-		area_3.modulate = Color.from_hsv(randf(), 0.8, 1.0)
