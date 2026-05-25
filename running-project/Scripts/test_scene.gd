0718f8e31b4530e4d1d2ab7ac9864e161e21329d
Muhammed Turhan <meheme4434@gmail.com>
2026-05-25 17:41:27 +0200

    ADDED simple 2D environment

--- running-project/Scripts/test_scene.gd
+++ running-project/Scripts/test_scene.gd
@@ -2,15 +2,9 @@ extends Node2D
 
 @export var rotation_speed: float = 1.0
 
-@onready var area_1 = $Area2D
+@onready var area_1 = $Coin
 @onready var area_2 = $Area2D2
 @onready var area_3 = $Area2D3
-
-
-func _process(delta: float) -> void:
-	area_1.rotation += rotation_speed * delta
-	area_2.rotation -= rotation_speed * delta
-	area_3.rotation += rotation_speed / 2 * delta
 
 
 func _input(event: InputEvent) -> void:
