37e3b49fea9b7bd431aef4dc21ad77c9c9531c6b
Muhammed Turhan <meheme4434@gmail.com>
2026-05-31 16:58:37 +0200

    ADDED slow down logic indipendent of frame rates

--- running-project/Scripts/player.gd
+++ running-project/Scripts/player.gd
@@ -4,13 +4,14 @@ extends CharacterBody2D
 const SPEED = 150.0
 
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_vector("left", "right", "up", "down")
 	if direction != Vector2.ZERO:
 		velocity = direction * SPEED
 	else:
-		velocity = velocity.move_toward(Vector2.ZERO, SPEED)
+		# Instead of instantly stopping, smoothly slow down the player independent of framerate.
+		velocity = velocity.move_toward(Vector2.ZERO, SPEED * 10 * _delta)
 
 	move_and_slide()
