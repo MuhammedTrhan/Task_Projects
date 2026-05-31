e86d6a019e08a69ffd3a0698e5898c092dd98114
Muhammed Turhan <meheme4434@gmail.com>
2026-05-31 19:38:35 +0200

    UPDATED animation logic of the player node

--- running-project/Scripts/player.gd
+++ running-project/Scripts/player.gd
@@ -16,3 +16,19 @@ func _physics_process(_delta: float) -> void:
 		velocity = velocity.move_toward(Vector2.ZERO, SPEED * 10 * _delta)
 
 	move_and_slide()
+
+	animation_manager()
+
+
+func animation_manager() -> void:
+	# play the walk animation if we're moving, otherwise play idle
+	# if velocity.length() > 0:
+	# 	$FlipPivot/AnimationPlayer.play("walk")
+	# else:
+	# 	$FlipPivot/AnimationPlayer.play("idle")
+	# flip the sprite based on movement direction
+	if velocity.x != 0:
+		if velocity.x < 0:
+			$FlipPivot.scale.x = -1
+		else:
+			$FlipPivot.scale.x = 1
