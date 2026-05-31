c27ffe4b691cef02924bb3fc451aa076318f322b
Muhammed Turhan <meheme4434@gmail.com>
2026-05-31 17:04:33 +0200

    REMOVED unnecessary assets

--- running-project/Scripts/player.gd
+++ running-project/Scripts/player.gd
@@ -9,6 +9,7 @@ func _physics_process(_delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_vector("left", "right", "up", "down")
 	if direction != Vector2.ZERO:
+		# move_and_slide is already framerate independent, so we don't need to use delta here.
 		velocity = direction * SPEED
 	else:
 		# Instead of instantly stopping, smoothly slow down the player independent of framerate.
